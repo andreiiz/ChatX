cbcf21cf30bee104d34a6bc49c967650732a45a9
Andrea Daskalopoulos <andrea.daskalopoulos@gmail.com>
2025-07-14 15:04:38 +0200

    first commit

---
+++ src/main/java/it/daska/chatx/ChatxApplication.java
@@ -0,0 +1,13 @@
+package it.daska.chatx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/daska/chatx/ChatxApplicationTests.java
@@ -0,0 +1,13 @@
+package it.daska.chatx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
